7bab48cf258659a421b859a2cb365811529c3896
JaeminBest <mikjm98@kaist.ac.kr>
2019-05-26 00:46:26 +0900

    ckpt

--- detection/__init__.py
+++ detection/__init__.py
@@ -39,17 +39,17 @@ def slope(line):
     # line is type of cv2.line
     for x1,y1,x2,y2 in line:
         if (x2==x1):
-            return -1
+            return None
         slope = (float)(y2-y1)/(float)(x2-x1)
         return slope
 
 # calculate distance(norm distance) between line(assumed to be straight line) and point(x,y)
 def distance(line,x,y):
-    p3 = (x,y)
+    p3 = np.array((x,y))
     for x1, y1, x2, y2 in line:
-        p1 = (x1, y1)
-        p2 = (x2, y2)
-    d=np.cross(p2-p1,p3-p1)/norm(p2-p1)
+        p1 = np.array((x1, y1))
+        p2 = np.array((x2, y2))
+    d=np.cross(p2-p1,p3-p1)/np.linalg.norm(p2-p1)
     return d
 
 # test if line2 can be extended to line1
@@ -69,9 +69,9 @@ def onsameline(line1,line2, slope_thld, dist_thld):
     
     for x1,y1,x2,y2 in line2:
         # check for possibility of estimation of furthest point to be on same line
-        if ((distnace(line1,x1,y1)>dist_thld) and (distance(line1, x2, y2)>dist_thld)):
+        if ((distance(line1,x1,y1)>dist_thld) or (distance(line1, x2, y2)>dist_thld)):
             return -1
-        return 1   
+    return 1   
 
 
 # new class for line sort
@@ -82,14 +82,32 @@ class el_line():
         self.line_y = []
         self.lines = []
         self.asymptote = []
+    
+    def __repr__(self):
+        return "< line_x : {}, line_y :{}, lines : {}, asymptote : {} >".format(self.line_x, self.line_y, self.lines, self.asymptote)
 
     def add(self,line):
         self.lines.append(line)
-        for x1, x2, y1, y2 in line:
-            self.line_x.extend([x1, x2])
-            self.line_y.extend([y1, y2])
-    
+        for x1, y1, x2, y2 in line:
+            if (x1 in self.line_x):
+                self.line_y[self.line_x.index(x1)] = (self.line_y[self.line_x.index(x1)]+y1)/2
+            else:
+                self.line_x.extend([x1])
+                self.line_y.extend([y1])
+            if (x2 in self.line_x):
+                self.line_y[self.line_x.index(x2)] = (self.line_y[self.line_x.index(x2)]+y2)/2
+            else:
+                self.line_x.extend([x2])
+                self.line_y.extend([y2])
+
     def estimate(self,img):
+        if (len(self.line_y)==2):
+            y1 = self.line_y[0]
+            y2 = self.line_y[1]
+            if (y1==y2):
+                self.asymptote = [[0,y1,img.shape[1],y1]]
+                return
+
         poly_left = np.poly1d(np.polyfit(
             self.line_y,
             self.line_x,
@@ -100,10 +118,11 @@ class el_line():
         left_x_start = (int)(poly_left(min_y))
         left_x_end = (int)(poly_left(max_y))
         self.asymptote = [[left_x_start,min_y,left_x_end,max_y]]
+        return
 
 
 # calibration of crosswalk
-def calib_crosswalk(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_thld=1, sort_thld = 0.2):
+def calib_crosswalk(img,region, threshold = 3, slope_thld=0.005, dist_thld=0.01, cnt_thld=1, sort_thld = 0.2):
     # region is list of 2 tuple (x,y) i.e. rectangular region described by diagonal 2 points
     # region of interest using region selected
     region_of_interest_vertices = [
@@ -225,7 +244,7 @@ def calib_crosswalk(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt
     return [left_line, right_line]
 
 # calibration of central line (yellow one)
-def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_thld=1, sort_thld = 0.2):
+def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=0.5, cnt_thld=1, sort_thld = 0.1):
     # region is list of tuple (x,y)
     # region of interest using region selected
     region_of_interest_vertices = [
@@ -238,54 +257,105 @@ def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_t
 
     # color detection
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    
-    # define range of blue color in HSV
-    lower_mask = np.array([11,50,50])
-    upper_mask = np.array([30,255,255])
-    mask = cv2.inRange(hsv, lower_mask, upper_mask)
-    cv2.imshow('0_central', img)
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(img,img, mask= mask)
-    cv2.imshow('1_central', res)
-    temp_img = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
-    cv2.imshow('2_central', temp_img)
-    gray_image = cv2.cvtColor(temp_img, cv2.COLOR_RGB2GRAY)
-    cv2.imshow('3_central', gray_image)
-    cannyed_image = cv2.Canny(gray_image, 100, 200, apertureSize=threshold)
-    
-    # crop)
-    cropped_image = region_of_interest(
-        cannyed_image,
-        np.array(
-            [region_of_interest_vertices],
-            np.int32
-        ),
-    )
-    cv2.imshow('4_central', cropped_image)
 
+    # extracting central+crosswalk
+    # using L*a*b*
+    lab = cv2.cvtColor(img,cv2.COLOR_BGR2Lab)
+    lower_mask1 = np.array([110,132,134])
+    upper_mask1 = np.array([255,255,255])
+    mask1 = cv2.inRange(lab, lower_mask1, upper_mask1)
+    res1 = cv2.bitwise_and(img,img, mask= mask1)
+    
+
+    img1 = cv2.cvtColor(res1,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('central_Lab filter',img1)
+
+    # extracting only crosswalk
+    # using YCrCb
+    ycrcb = cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
+    lower_mask2 = np.array([150,0,0])
+    upper_mask2 = np.array([255,225,115])
+    mask2 = cv2.inRange(ycrcb, lower_mask2, upper_mask2)
+    res2 = cv2.bitwise_and(img,img, mask= mask2)
+    
+
+    img2 = cv2.cvtColor(res2,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('central_YCrCb filter',img2)
+
+    # subtraction, extracting ONLY central line
+    new_img = np.subtract(img1,img2)
+    new_img = np.where(img1-img2<0,0,img1-img2)
+    cv2.imshow('central_extract', new_img)
+
+    # define range of blue color in HSV
+    kernel1 = np.ones((5, 5), np.uint8)
+    morp1 = cv2.morphologyEx(new_img, cv2.MORPH_OPEN, kernel1)
+    cv2.imshow('central_extract_open', morp1)
+    kernel2 = np.ones((5, 5), np.uint8)
+    morp2 = cv2.morphologyEx(morp1, cv2.MORPH_CLOSE, kernel2)
+    cv2.imshow('central_extract_open_close', morp2)
+    kernel3 = np.ones((5, 5), np.uint8)
+    morp3 = cv2.erode(morp2, kernel3, iterations = 1)
+    cv2.imshow('central_extract_open_close_erode', morp3)
+    edges = cv2.Canny(morp3, 100, 200, apertureSize=3)
+    cv2.imshow('central_final edge', edges)
     # line detection
     lines = cv2.HoughLinesP(
-        cropped_image,
+        morp3,
         rho=6,
         theta=np.pi / 60,
-        threshold=20,
+        threshold=200,
         lines=np.array([]),
-        minLineLength=10,
+        minLineLength=100,
         maxLineGap=25
     )
+
+    lines1 = cv2.HoughLines(morp3,1,np.pi/180,100)
+
+    line_temp_image = draw_lines(
+        img,
+        lines,
+        color = [0,0,255],
+        thickness=5,
+    )
+    #cv2.imshow('img_hough',line_temp_image)
+
+    for i in range(len(lines1)):
+        for rho, theta in lines1[i]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + img.shape[1]*3*(-b))
+            y1 = int(y0+img.shape[1]*3*(a))
+            x2 = int(x0 - img.shape[1]*3*(-b))
+            y2 = int(y0 -img.shape[1]*3*(a))
+
+            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+    res = np.vstack((img.copy(), img))
+    cv2.imshow('img',res)
+
+
 
     lines_sort = []
     #print(lines)
     #print("======")
     # sort lines by slope and gather using class el_line
+    print('iteration start')
     for line in lines:
+        print('setting iteration')
         #print(line)
         sl = slope(line)
+        if (sl is None):
+            continue
         # zero division set by -1 therefore no count!
-        if ((sl < 0) or (sl>=1)):
+        if ((sl > 0) or (sl<=-1)):
+            print('invalid slope')
             continue
             
         if (not lines_sort):
+            print('new line')
             temp = el_line()
             temp.add(line)
             temp.estimate(img)
@@ -294,33 +364,53 @@ def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_t
         else:
             sort_flag = 0
             for el in lines_sort:
-                if onsameline(el.asymptote,line,slope_thld,dist_thld):
+                if (onsameline(el.asymptote,line,slope_thld,dist_thld)==1):
+                    print('line extension')
                     el.add(line)
                     el.estimate(img)
                     sort_flag = 1
             if (not sort_flag):
+                print('new line')
                 temp = el_line()
                 temp.add(line)
                 temp.estimate(img)
                 lines_sort.append(temp)
+    
+    print("before filter")
+    for el in lines_sort:
+        print(el)
 
     # delete lines_sort based on count threshold
     # which means delete less-detected slope of lines
     avg_slope = 0
     tot_count = 0
     for el in lines_sort:
-        if (len(el.line_x)<cnt_thld):
+        if ((slope(el.asymptote)>0) or (slope(el.asymptote)<=-1)):
+            print('remove')
             lines_sort.remove(el)
         else:
             avg_slope += slope(el.asymptote)
             tot_count += 1
-    avg_slope = avg_slope/tot_count
+    
+    # slope filtering by active region
+    for el in lines_sort:
+        temp_line = el.asymptote
+        for x1,y1,x2,y2 in temp_line:
+            if (((x1<img.shape[1]) and (x1>0)) or ((x2<img.shape[1]) and (x2>0))):
+                lines_sort.remove(el)
+            elif ((x1<0) or (x2>0)):
+                lines_sort.remove(el)
+    
+    #if (tot_count!=0):
+    #    avg_slope = avg_slope/tot_count
+    #else:
+    #    avg_slope = None
     
     # delete lines_sort based on sort threshold
     # which means delete lines of thought to be noise slope
-    for el in lines_sort:
-        if (math.fabs(slope(el.asymptote)-avg_slope)>=sort_thld):
-            lines_sort.remove(el)
+    #for el in lines_sort:
+    #    if (math.fabs(slope(el.asymptote)-avg_slope)>=sort_thld):
+    #        lines_sort.remove(el)
 
     left_line = lines_sort[0].asymptote
     right_line = lines_sort[0].asymptote
@@ -332,6 +422,10 @@ def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_t
         if (slope(right_line)<slope(el.asymptote)):
             right_line = el.asymptote
 
+    print("after filter")
+    for el in lines_sort:
+        print(el)
+
     line_image = draw_lines(
         img,
         [[
@@ -340,14 +434,6 @@ def calib_central(img,region, threshold = 3, slope_thld=0.05, dist_thld=2, cnt_t
         ]],
         thickness=5,
     )
-
-    #for line in lines:
-    #    line_image = draw_lines(
-    #        line_image,
-    #        [line],
-    #        color = [0,0,255],
-    #        thickness=5,
-    #    )
 
     cv2.imshow('img_central',line_image)
     
@@ -368,8 +454,8 @@ def calibration(datadir=datadir, threshold=3):
     height = img.shape[0]
     width = img.shape[1]
     region1 = [
-        (0, height),
-        (width, height / 2)
+        (width / 2, height * 2/3),
+        (width, height * 1/3)
     ]
     region2 = [
         (width * 4/7, height * 3/7),
---
+++ detection/cnn.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import os
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import math
