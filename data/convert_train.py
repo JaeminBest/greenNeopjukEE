098463a3ce8632b7c17361ebcc7fca32bf5f5f64
di-uni <50884017+di-uni@users.noreply.github.com>
2019-05-29 19:03:34 +0900

    convert train data path

---
+++ data/convert_train.py
@@ -0,0 +1,18 @@
+import sys
+
+try:
+	input = open(sys.argv[1], "r")
+except:
+	print ('ERROR: check the input file name')
+	sys.exit(1)
+
+while True:
+	line = input.readline()
+	if not line:
+		break
+	output = open("convert_train.txt", "a")
+	line = line.replace("x64/Release", "/Users/jiyun/darknet")
+	print (line)
+	output.write(line)
+
+input.close()
